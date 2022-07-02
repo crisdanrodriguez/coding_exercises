ed57066010b522d94c19c9d4153314c02350c017
Cristian <cristian.714@hotmail.com>
2022-07-01 21:43:45 -0500

    Added palindrome exercise

---
+++ strings/c++/palindrome_check.cpp
@@ -0,0 +1,17 @@
+/*
+Write a function that takes in a non-empty string and that returns a boolean representing whether the 
+string is a palindrome.
+
+A palindrome is defined as a string that is written the same forward and backward. Note that single
+character strings are palindromes.
+*/
+
+using namespace std;
+
+bool is_palindrome(string str) {
+    string reversedString = "";
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return str == reversedString;
+}
